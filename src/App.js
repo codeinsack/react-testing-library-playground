05bdcb31afa1a1465d3b19bf5da5a8c7f2714d66
codeinsack <codeinsack@gmail.com>
2022-03-27 17:52:44 +0300

    style NavBar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from "./pages/Login/Login";
 import User from "./pages/User/User";
 import { useTranslation } from "react-i18next";
 import { useState } from "react";
+import logo from "./assets/hoaxify.png";
 
 function App() {
   const { t } = useTranslation();
@@ -12,30 +13,52 @@ function App() {
 
   const onLinkClick = (event) => {
     event.preventDefault();
-    const path = event.target.attributes.href.value;
+    const path = event.currentTarget.attributes.href.value;
     window.history.pushState({}, "", path);
     setPath(path);
   };
 
   return (
-    <div className="container">
-      <div>
-        <a href="/" title="Home" onClick={onLinkClick}>
-          {t("home")}
-        </a>
-        <a href="/signup" title="Sign Up" onClick={onLinkClick}>
-          {t("signUp")}
-        </a>
-        <a href="/login" title="Login" onClick={onLinkClick}>
-          {t("login")}
-        </a>
+    <>
+      <nav className="navbar navbar-expand navbar-light bg-light shadow-sm">
+        <div className="container">
+          <a
+            className="navbar-brand"
+            href="/"
+            title="Home"
+            onClick={onLinkClick}
+          >
+            <img src={logo} width="60" alt="Hoaxify" />
+            Hoaxify
+          </a>
+          <ul className="navbar-nav">
+            <a
+              className="nav-link"
+              href="/signup"
+              title="Sign Up"
+              onClick={onLinkClick}
+            >
+              {t("signUp")}
+            </a>
+            <a
+              className="nav-link"
+              href="/login"
+              title="Login"
+              onClick={onLinkClick}
+            >
+              {t("login")}
+            </a>
+          </ul>
+        </div>
+      </nav>
+      <div className="container">
+        {path === "/" && <Home />}
+        {path === "/signup" && <SignUp />}
+        {path === "/login" && <Login />}
+        {path.startsWith("/user/") && <User />}
+        <LanguageSelector />
       </div>
-      {path === "/" && <Home />}
-      {path === "/signup" && <SignUp />}
-      {path === "/login" && <Login />}
-      {path.startsWith("/user/") && <User />}
-      <LanguageSelector />
-    </div>
+    </>
   );
 }
 
