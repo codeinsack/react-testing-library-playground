6a48ead2db2f3cce3341a70f9d337ae9ecb2a90e
codeinsack <codeinsack@gmail.com>
2022-03-30 13:05:08 +0300

    update HOC

--- src/components/LanguageSelector/LanguageSelector.jsx
+++ src/components/LanguageSelector/LanguageSelector.jsx
@@ -1,22 +1,27 @@
 import { useTranslation } from "react-i18next";
 import withHover from "../../HOCs/withHover";
 
-const LanguageSelector = () => {
+const LanguageSelector = ({ on }) => {
   const { i18n } = useTranslation();
+
+  let size = 24;
+  if (on) {
+    size = 48;
+  }
 
   return (
     <>
       <img
-        src="http://purecatamphetamine.github.io/country-flag-icons/3x2/RU.svg"
+        src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/RU.svg`}
         title="Russian"
-        width="50"
+        width={size}
         alt="Russian"
         onClick={() => i18n.changeLanguage("ru")}
       />
       <img
         src="http://purecatamphetamine.github.io/country-flag-icons/3x2/GB.svg"
         title="English"
-        width="50"
+        width={size}
         alt="English"
         onClick={() => i18n.changeLanguage("en")}
       />
